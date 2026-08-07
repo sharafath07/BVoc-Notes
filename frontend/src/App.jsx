b76a7d8c44e18c7e29686458b5b3d18019660054
Sharafath Ahammed V <142240872+sharafath07@users.noreply.github.com>
2026-08-07 16:35:27 +0000

    Server created and done basic setups in frontend and backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='text-3xl font-bold underline text-blue-900'>
+      Hello BVOCians!
+    </div>
+  )
+}
+
+export default App
